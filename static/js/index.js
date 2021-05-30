83b8c3a6ce4db78987e9f87b4afb85d8e6c2d643
johnny <vmjuan90@gmail.com>
2021-05-30 11:22:32 +0200

    estructura añadida

---
+++ static/js/index.js
@@ -0,0 +1,15 @@
+function load_content(path) {
+	fetch(path, { mode: "no-cors", }).then(function(response) {
+	  if(response.ok) {
+		document.getElementsByTagName("main")[0].innerHTML = response.text();
+	  } else {
+	    console.log('Net OK but HTTP Error');
+	  }
+	})
+	.catch(function(error) {
+	  console.log('Request Fetch error:' + error.message);
+	});
+}
+
+/* First action: Load main page*/
+load_content("static/html/main.html");
