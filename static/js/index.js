1699d0e1341b20b496363080c9ecc92039408819
johnny <vmjuan90@gmail.com>
2021-05-31 23:09:26 +0200

    Reorganizated and added buttons

--- static/js/index.js
+++ static/js/index.js
@@ -11,5 +11,34 @@ function load_content(path) {
 	});
 }
 
+/* Set events listeners */
+document.querySelector("header .logo")
+.addEventListener('click', event => {
+	load_content("static/html/main.html");
+});
+
+document.querySelector("header #integration")
+.addEventListener('click', () => {
+	load_content("static/html/integrations.html");
+});
+
+document.querySelector("header #erp-odoo")
+.addEventListener('click', () => {
+	load_content("static/html/erp_odoo.html");
+});
+
+document.querySelector("header #apps")
+.addEventListener('click', () => {
+	load_content("static/html/apps.html");
+});
+
+document.querySelectorAll("header .contact-us")
+.forEach(item => {
+	item.addEventListener('click', () => {
+		load_content("static/html/contactus.html");
+	});
+});
+
 /* First action: Load main page*/
 load_content("static/html/main.html");
+
