f51158e9d5d9543d7e896710fb65f0a7e44a6f1f
johnny <vmjuan90@gmail.com>
2021-06-12 22:10:22 +0200

    update image

--- run-server.sh
+++ run-server.sh
@@ -2,6 +2,14 @@
 
 set -ue
 
+usage() {
+	echo "$0 [-h] [-p port_number]"
+}
+
+# todo add command line option for set port
+PORT_NUMBER_DEFAULT=8080
+PORT_NUMBER=${PORT_NUMBER:-${PORT_NUMBER_DEFAULT}}
+
 if command -v podman > /dev/null 2>&1; then
 	CONTAINER_ENGINE=podman
 elif command -v docker > /dev/null 2>&1; then
@@ -12,7 +20,7 @@ else
 fi
 
 "${CONTAINER_ENGINE}" run --rm -d \
-    --name guadalsistema-nginx \
-    -p "8080:80" \
-	-v "$PWD/www":/usr/share/nginx/html:ro \
-    nginx
+	--name www-nginx \
+	-p "${PORT_NUMBER}:80" \
+	-v "${PWD}"/www:/usr/share/nginx/html:ro \
+	nginx
