453aa283c9904e04c01fc70c551e135ec533688a
johnny <vmjuan90@gmail.com>
2021-06-13 00:39:46 +0200

    wip

--- run-server.sh
+++ run-server.sh
@@ -2,13 +2,33 @@
 
 set -ue
 
+PORT_NUMBER_DEFAULT=8080
+CONTAINER_NAME_DEFAULT=www-nginx
+
 usage() {
-	echo "$0 [-h] [-p port_number]"
+	printf "%s [-h] [-p port_number] [-n container_name]\n" "$(basename "$0")"
+	cat << EOF
+Flags usage:
+	-h display help and exit
+	-p set the port for the server. default: ${PORT_NUMBER_DEFAULT}
+	-n set the name for the container. default: ${CONTAINER_NAME_DEFAULT}
+EOF
 }
 
+is_number='^[0-9]+$'
+while getopts "hvp:n:" flag
+do
+	case "$flag" in
+		h) usage; exit 0;;
+		p) if [ ${OPTARG} =~ is_number ]; then PORT_NUMBER=${OPTARG};;
+		n) CONTAINER_NAME="${OPTARG}";;
+		*) usage; exit 0;;
+	esac
+done
+
 # todo add command line option for set port
-PORT_NUMBER_DEFAULT=8080
 PORT_NUMBER=${PORT_NUMBER:-${PORT_NUMBER_DEFAULT}}
+CONTAINER_NAME=${CONTAINER_NAME:-${CONTAINER_NAME_DEFAULT}}
 
 if command -v podman > /dev/null 2>&1; then
 	CONTAINER_ENGINE=podman
@@ -20,7 +40,7 @@ else
 fi
 
 "${CONTAINER_ENGINE}" run --rm -d \
-	--name www-nginx \
+	--name "${CONTAINER_NAME}" \
 	-p "${PORT_NUMBER}:80" \
 	-v "${PWD}"/www:/usr/share/nginx/html:ro \
 	nginx
